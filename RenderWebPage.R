71d5b04d2971193dd45a44f97718d876bf7c3c19
Rob Carnell <bertcarnell@gmail.com>
2025-07-02 16:24:17 -0400

    Added webpage

---
+++ RenderWebPage.R
@@ -0,0 +1,7 @@
+rmarkdown::render(input = "SolarBusinessCase.Rmd",
+                  output_format = "html_document",
+                  output_file = "SolarBusinessCase.html",
+                  output_dir = "docs",
+                  knit_root_dir = getwd())
+
+
